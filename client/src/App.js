8f9260eba9391518692d0a56a6b8da969a578b90
danielrubens <danielrubensdaniel@gmail.com>
2023-02-05 20:50:05 -0300

    NotesList page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Header from './components/Header';
-
+import NotesList from './pages/NotesList';
 function App() {
   return (
     <div className="App">
       <Header />
+      <NotesList />
     </div>
   );
 }
