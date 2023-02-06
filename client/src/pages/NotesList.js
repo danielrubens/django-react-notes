8d12aa350ac259715d90c1a72bc756a94050ea56
danielrubens <danielrubensdaniel@gmail.com>
2023-02-05 22:50:34 -0300

    server notes urls path prefix changed to api/

--- client/src/pages/NotesList.js
+++ client/src/pages/NotesList.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 const NotesList = () => {
+  const [notes, setNotes] = useState([])
+
+  useEffect(() => {}, [])
+
+  const getNotes = async () => {
+    fetch('http://127.0.0.1:8000/notes/').then((res) => res.json()).then((data) => setNotes(data))
+  }
+
   return (
-    <div>NotesList</div>
+    <div>{notes.map((i) => (<p>{console.log(i)}</p>))}</div>
   )
 }
 
